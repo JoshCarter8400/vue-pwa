256499ab612a5a366af597cb4126c964368066c1
Josh Carter <joshcarter8400@gmail.com>
2021-02-27 15:23:25 -0500

    added in card for posts along with styles for the card

--- vue-pwa/src/pages/PageHome.vue
+++ vue-pwa/src/pages/PageHome.vue
@@ -1,6 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Page</h5>
+  <q-page class="constrain q-pa-md">
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">Josh Carter</q-item-label>
+          <q-item-label caption>
+            Key West, FL
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-card-section>
+        <div>VueGram is the best!!</div>
+        <div class="text-caption text-grey">Feb. 27th 2021</div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
@@ -9,3 +32,9 @@ export default {
   name: "PageHome"
 };
 </script>
+
+<style lang="sass">
+.card-post
+  .q-img
+    min-height: 200px
+</style>
