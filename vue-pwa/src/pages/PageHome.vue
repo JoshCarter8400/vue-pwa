9b4c1a122d4db72dd9c4944da0e167f0938213ed
Josh Carter <joshcarter8400@gmail.com>
2021-02-27 16:12:15 -0500

    added mini profile section and and styles with more responsiveness

--- vue-pwa/src/pages/PageHome.vue
+++ vue-pwa/src/pages/PageHome.vue
@@ -1,35 +1,101 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card class="card-post" flat bordered>
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-          </q-avatar>
-        </q-item-section>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          class="card-post q-mb-md"
+          flat
+          bordered
+          v-for="post in posts"
+          :key="post.id"
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-bold">Josh Carter</q-item-label>
-          <q-item-label caption>
-            Key West, FL
-          </q-item-label>
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              <q-item-label class="text-bold">Josh Carter</q-item-label>
+              <q-item-label caption>
+                {{ post.location }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-separator />
+          <q-separator />
 
-      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" />
-      <q-card-section>
-        <div>VueGram is the best!!</div>
-        <div class="text-caption text-grey">Feb. 27th 2021</div>
-      </q-card-section>
-    </q-card>
+          <q-img :src="post.imageUrl" />
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">Josh Carter</q-item-label>
+            <q-item-label caption>
+              Josh Carter
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import { date } from "quasar";
 export default {
-  name: "PageHome"
+  name: "PageHome",
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: "VueGram is the best!!",
+          date: 1614457611983,
+          location: "Key West, FL",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg"
+        },
+        {
+          id: 2,
+          caption: "VueGram is the best!!",
+          date: 1614457611983,
+          location: "Key West, FL",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg"
+        },
+        {
+          id: 3,
+          caption: "VueGram is the best!!",
+          date: 1614457611983,
+          location: "Key West, FL",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg"
+        },
+        {
+          id: 4,
+          caption: "VueGram is the best!!",
+          date: 1614457611983,
+          location: "Key West, FL",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg"
+        }
+      ]
+    };
+  },
+  filters: {
+    niceDate(value) {
+      return date.formatDate(value, "MMMM D h:mmA");
+    }
+  }
 };
 </script>
 
